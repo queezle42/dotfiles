cc2cd688efe44fb94b18ad634f221e0cc03f9ff7
Jens Nolte <jens@nightmarestudio.de>
2019-10-22 14:09:30 +0200

    Add steam layer

---
+++ nixos/layers/steam.nix
@@ -0,0 +1,25 @@
+{ lib, pkgs, ... }:
+
+with lib;
+
+let
+  customSteam = pkgs.steam.override {
+    withPrimus = true;
+    extraPkgs = pkgs: with pkgs; [ glxinfo ];
+  };
+
+in
+{
+  hardware.opengl.driSupport32Bit = true;
+  hardware.pulseaudio.support32Bit = true;
+
+  users.users.steam = {
+    isNormalUser = true;
+    uid = 1100;
+    passwordFile = "/q/passwords/steam";
+    extraGroups = [ "audio" "input" ];
+    packages = [
+      customSteam
+    ];
+  };
+}
