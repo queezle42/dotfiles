29afdc5c149af0b374dde8bbd4d73c35490496d5
Jens Nolte <jens@nightmarestudio.de>
2019-10-22 14:06:13 +0200

    Create parameterized top-level entry point

--- nixos/default.nix
+++ nixos/default.nix
@@ -1,9 +1,14 @@
+# This is the entry point for my NixOS configuration.
+{ layers ? [] }:
 { lib, config, pkgs, ... }:
 
+let
+  layerImports = map (l: ./layers + "/${l}.nix") layers;
+in
 {
   imports = [
     ./modules
-  ];
+  ] ++ layerImports;
 
   nixpkgs.config = {
     packageOverrides = ( import ./pkgs ) { inherit lib config; } ;
