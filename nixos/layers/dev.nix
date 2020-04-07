4ca374b62d297f9a4931abd26613a9096359411d
Jens Nolte <jens@nightmarestudio.de>
2020-04-07 15:09:09 +0200

    Add 'dev' layer that adds dev user

---
+++ nixos/layers/dev.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }:
+
+{
+  environment.systemPackages = with pkgs; [
+  ];
+
+  users = {
+    users.dev = {
+      uid = 1300;
+      isNormalUser = true;
+      packages = with pkgs; [
+      ];
+      dotfiles.profiles = [ "kitty" "vscode" ];
+    };
+  };
+}
