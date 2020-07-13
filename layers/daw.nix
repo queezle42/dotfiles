eeffa369e12fb4e86b5592cd0fef78625e7e793d
Jens Nolte <jens@nightmarestudio.de>
2020-07-14 00:09:04 +0200

    Add daw layer for audo production

---
+++ layers/daw.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }:
+
+{
+  users.groups."pulse-access" = {};
+  users.users.audio = {
+    isNormalUser = true;
+    uid = 1102;
+    extraGroups = [ "pulse-access" "audio" ];
+    packages = with pkgs; [
+      ardour
+      calf
+      vcv-rack
+      #sunvox
+    ];
+  };
+}
