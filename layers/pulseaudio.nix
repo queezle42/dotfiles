273abf96de6e227e3a7b49547d3b1862f8565523
Jens Nolte <git@queezle.net>
2021-09-11 22:51:06 +0200

    Prevent Scarlett 2i4 from being added to pulseaudio by udev

--- layers/pulseaudio.nix
+++ layers/pulseaudio.nix
@@ -19,4 +19,12 @@
   system.activationScripts.fix-pulse-permissions = ''
     chmod 755 /run/pulse
   '';
+
+
+  # Focusrite Scarlett 2i4
+  # Sample rate switching (e.g. when starting mumble) results in weird glitches; stereo profile can't be applied.
+  # It has to be manually configured instead.
+  services.udev.extraRules = ''
+    ENV{ID_VENDOR_ID}=="1235", ENV{ID_MODEL_ID}=="8200", ENV{PULSE_IGNORE}="1"
+  '';
 }
