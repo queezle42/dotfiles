5825159634283f413aa3bd17ef96de71c22f953a
Jens Nolte <git@queezle.net>
2021-09-12 00:04:27 +0200

    Add udev-triggered service to correctly load Scarlett

--- layers/pulseaudio.nix
+++ layers/pulseaudio.nix
@@ -1,4 +1,8 @@
-{ pkgs, ... }:
+{ config, lib, pkgs, ... }:
+with lib;
+let
+  pactl-bin = "${pkgs.pulseaudio}/bin/pactl";
+in
 {
   sound.enable = true;
   hardware.pulseaudio = {
@@ -36,5 +40,28 @@
   # It has to be manually configured instead.
   services.udev.extraRules = ''
     ENV{ID_VENDOR_ID}=="1235", ENV{ID_MODEL_ID}=="8200", ENV{PULSE_IGNORE}="1"
+    SUBSYSTEMS=="usb", ATTRS{idVendor}=="1235", ATTRS{idProduct}=="8200", TAG+="systemd", ENV{SYSTEMD_WANTS}="scarlett-2i4.service"
   '';
+  # Remove if above works
+  # SUBSYSTEM=="usb", ACTION=="add", ENV{ID_VENDOR_ID}=="1235", ENV{ID_MODEL_ID}=="8200", RUN+="${scarlett-bin}"
+
+  systemd.services.scarlett-2i4 = {
+    restartIfChanged = false;
+    after = [ "pulseaudio.service" ];
+    script = ''
+      ${pactl-bin} load-module module-alsa-card device_id=USB card_name=scarlett card_properties=device.description=Scarlett sink_name=scarlett-sink source_name=scarlett-source format=s32le rate=48000 use_ucm=true
+
+      ${pactl-bin} load-module module-remap-sink sink_name=scarlett-sink-stereo sink_properties=device.description=Scarlett_Stereo master=scarlett-sink channels=2 master_channel_map=left,right remix=no
+
+      ${pactl-bin} set-default-sink scarlett-sink-stereo
+      ${pactl-bin} set-default-source scarlett-source
+    '';
+    unitConfig = {
+      Description = "Add Focusrite Scarlett 2i4 to PulseAudio";
+    };
+    serviceConfig = {
+      Type = "simple";
+      User = "pulse";
+    };
+  };
 }
