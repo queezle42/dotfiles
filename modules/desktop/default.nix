a1c034adc6b8df8ece9849c515be287283fd0d19
Jens Nolte <git@queezle.net>
2021-11-19 04:05:25 +0100

    Deploy squeekboard layout

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -3,6 +3,7 @@ inputs@{ lib, pkgs, config, ... }:
 with lib;
 let
   cfg = config.queezle.desktop;
+  squeekboardConfig = import ./config/squeekboard.nix inputs;
 in
 {
   options = {
@@ -18,8 +19,20 @@ in
     };
   };
   config = mkIf cfg.enable {
-    home-manager.users."${cfg.user}".xdg.configFile."foot/foot.ini" = {
-      source = import ./config/foot.nix inputs;
+    queezle.terminal.enable = true;
+
+    home-manager.users."${cfg.user}".xdg = {
+      configFile."foot/foot.ini" = {
+        source = import ./config/foot.nix inputs;
+      };
+      dataFile."squeekboard/keyboards/terminal/us.yaml" = { source = squeekboardConfig; };
+      dataFile."squeekboard/keyboards/terminal/us_wide.yaml" = { source = squeekboardConfig; };
+      dataFile."squeekboard/keyboards/us.yaml" = { source = squeekboardConfig; };
+      dataFile."squeekboard/keyboards/us_wide.yaml" = { source = squeekboardConfig; };
+
+      # TODO: remove after next nixpkgs-pinephone update
+      dataFile."squeekboard/keyboards/terminal.yaml" = { source = squeekboardConfig; };
+      dataFile."squeekboard/keyboards/terminal_wide.yaml" = { source = squeekboardConfig; };
     };
   };
 }
