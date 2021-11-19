676a7367ecd93ec468470bd2beefee60ec6f29d6
Jens Nolte <git@queezle.net>
2021-11-19 03:53:02 +0100

    Install hoogle service; remove stack

--- layers/dev-haskell.nix
+++ layers/dev-haskell.nix
@@ -3,10 +3,21 @@
 {
   environment.systemPackages = with pkgs; [
     cabal-install
-    stack
     #haskell-language-server
     haskellPackages.hoogle
     ghcid
     #haskellPackages.threadscope
   ];
+
+  services.hoogle = {
+    enable = true;
+    packages = hp: with hp; [
+      hashable
+      heaps
+      network
+      #quasar
+      #quasar-network
+      unordered-containers
+    ];
+  };
 }
