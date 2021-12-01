028088aafb273fec172d0b00c4f49e1243b5de1c
Jens Nolte <git@queezle.net>
2021-12-01 22:45:58 +0100

    Add g815-led binary for g810-led package

--- pkgs/g810-led/default.nix
+++ pkgs/g810-led/default.nix
@@ -9,14 +9,15 @@ stdenv.mkDerivation {
     rev = "5ee810a520f809e65048de8a8ce24bac0ce34490";
     sha256 = "1ymkp7i7nc1ig2r19wz0pcxfnpawkjkgq7vrz6801xz428cqwmhl";
   };
-  
+
   makeFlags = ["DESTDIR=$(out)" "PREFIX=$(out)"];
-  
+
   buildInputs = [hidapi];
   nativeBuildInputs = [tree];
 
   installPhase = ''
     mkdir -p $out/bin
     cp bin/g810-led $out/bin/
+    ln -s $out/bin/g810-led $out/bin/g815-led
   '';
 }
