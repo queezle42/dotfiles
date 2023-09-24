bd6dee088e6c4eaef1dd4204fc1221218c37f75f
Jens Nolte <git@queezle.net>
2023-09-25 01:44:49 +0200

    Add serial device for hdmi matrix switcher

--- layers/tv.nix
+++ layers/tv.nix
@@ -2,11 +2,20 @@
 with lib;
 
 {
-  services.udev.packages = singleton (pkgs.writeTextFile {
-    name = "tv-serial-udev-rules";
-    destination = "/etc/udev/rules.d/90-tv-serial.rules";
-    text = ''
-      SUBSYSTEM=="tty", ATTRS{idVendor}=="067b", ATTRS{idProduct}=="2303", SYMLINK+="tty-tv"
-    '';
-  });
+  services.udev.packages = [
+    (pkgs.writeTextFile {
+      name = "tv-serial-udev-rules";
+      destination = "/etc/udev/rules.d/90-tv-serial.rules";
+      text = ''
+        SUBSYSTEM=="tty", ATTRS{idVendor}=="1a86", ATTRS{idProduct}=="7523", SYMLINK+="tty-tv"
+      '';
+    })
+    (pkgs.writeTextFile {
+      name = "hdmi-matrix-serial-udev-rules";
+      destination = "/etc/udev/rules.d/90-hdmi-matrix-serial.rules";
+      text = ''
+        SUBSYSTEM=="tty", ATTRS{idVendor}=="067b", ATTRS{idProduct}=="2303", SYMLINK+="tty-hdmi-matrix"
+      '';
+    })
+  ];
 }
